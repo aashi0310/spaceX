5e8402d34c8beec483084f91e616e784b4c66115
Akanksha Sharma <sharma03aashi@gmail.com>
2021-01-24 22:57:59 +0530

    CSS update

--- src/App.test.js
+++ src/App.test.js
@@ -21,6 +21,27 @@ afterEach(() => {
     wrapper.unmount();
  });
 
-it("must render a loading div before api call success", () => {
-    
-});
+ describe('App', () => {
+    it('fetches data from server when server returns a successful response', done => { // 1
+      const mockSuccessResponse = {};
+      const mockJsonPromise = Promise.resolve(mockSuccessResponse); // 2
+      const mockFetchPromise = Promise.resolve({ // 3
+        json: () => mockJsonPromise,
+      });
+      jest.spyOn(global, 'fetchAPI').mockImplementation(() => mockFetchPromise); // 4
+      
+      const wrapper = shallow(<App />); // 5
+                              
+      expect(global.fetch).toHaveBeenCalledTimes(1);
+      expect(global.fetch).toHaveBeenCalledWith('https://api.spacexdata.com/v3/launches?limit=100limit=150&launch_year=&launch_success=&land_success=');
+  
+      process.nextTick(() => { // 6
+        expect(wrapper.state()).toEqual({
+          // ... assert the set state
+        });
+  
+        global.fetch.mockClear(); // 7
+        done(); // 8
+      });
+    });
+  });
